3b17b0028e3b47ddf0a2168dd59ddd1b0f206f39
Hatemii <hatem.xhezairi96@gmail.com>
2022-05-08 14:56:16 +0200

    User.rb comment JwtDenylist

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :jwt_authenticatable,
          :registerable,
-         jwt_revocation_strategy: JwtDenylist
+        #  jwt_revocation_strategy: JwtDenylist
 end
