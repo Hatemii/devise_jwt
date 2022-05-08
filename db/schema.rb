d8a29f0dc89c5493993b4793e29211c02840b332
Hatemii <hatem.xhezairi96@gmail.com>
2022-05-08 15:19:52 +0200

    add JwtDenylist for revoking JWT

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_08_124240) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_08_131601) do
   create_table "api_posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "expire", null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
   end
 
   create_table "users", force: :cascade do |t|
