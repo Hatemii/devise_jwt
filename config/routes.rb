6f0bd08958abc99727aa3245980b06f7c315a8b6
Hatemii <hatem.xhezairi96@gmail.com>
2022-05-08 22:59:54 +0200

    current_user_controller added

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :posts
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/current_user', to: 'current_user#index'
+
 end
