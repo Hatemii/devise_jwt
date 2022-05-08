73915193effd1113771a64f82bf9d17ea4f5b00d
Hatemii <hatem.xhezairi96@gmail.com>
2022-05-08 15:22:01 +0200

    why JwtDenylist

--- app/models/jwt_denylist.rb
+++ app/models/jwt_denylist.rb
@@ -1,4 +1,7 @@
 class JwtDenylist < ApplicationRecord
+  # This table is used as a list of revoked JWT tokens 
+  # that have been logged out and no longer have access to our application.
+  
   include Devise::JWT::RevocationStrategies::Denylist
 
   self.table_name = 'jwt_denylist'
