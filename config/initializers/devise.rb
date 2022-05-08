59b7ce3708373c2392b58738ea98fe92db9f5ea0
Hatemii <hatem.xhezairi96@gmail.com>
2022-05-08 22:12:28 +0200

    devise.rb refactor

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -322,5 +322,12 @@ Devise.setup do |config|
   # generate secret key with command "rails secret on terminal"
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 30.minutes.to_i
   end
 end
